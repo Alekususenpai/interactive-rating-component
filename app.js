c64aabb98945466829608464705482142e4ceef9
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-03-25 00:37:03 +0100

    click rating logic

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-let rating = document.getElementById('rating')
+let ratings = document.querySelectorAll('.circle');
+let btn = document.getElementById('btn');
+let evaluation = document.querySelector('.evaluation');
+let thankyou = document.querySelector('.thankyou');
+let span = document.getElementById('rating');
 
-function rate(){
-
+for (let i = 0; i < ratings.length; i++) {
+          ratings[i].addEventListener('click', () => {
+                    let rating = ratings[i].innerHTML;
+                    btn.addEventListener('click', () => {
+                              evaluation.style.display = 'none';
+                              thankyou.style.display = 'block';
+                              span.innerHTML = rating;
+                    })
+          })
 }
