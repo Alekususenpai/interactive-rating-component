de3d6c323486dd8f9c7e2f392eb8406e0554b3d0
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-03-30 00:34:06 +0200

    clear previous selected ratings

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ let span = document.getElementById('rating');
 
 for (let i = 0; i < ratings.length; i++) {
           ratings[i].addEventListener('click', () => {
-
-                    ratings.classList.remove('selected');
-                    
                     let rating = ratings[i].innerHTML;
-                    ratings[i].classList.add('selected');
+
+                    for (let i = 0; i < ratings.length; i++) {
+                              ratings[i].classList.contains('selected')
+                              ratings[i].classList.remove('selected');
+                    }
+
+                    ratings[i].classList.toggle('selected');
 
                     btn.addEventListener('click', () => {
                               evaluation.style.display = 'none';
